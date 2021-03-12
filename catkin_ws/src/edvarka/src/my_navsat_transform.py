0d15def4732d88a582dee65598540852a09b077d
tsioftas <panaevri@gmail.com>
2021-03-12 17:21:45 +0000

    added longlat_to_xy method to my_navsat_transform

--- catkin_ws/src/edvarka/src/my_navsat_transform.py
+++ catkin_ws/src/edvarka/src/my_navsat_transform.py
@@ -15,29 +15,16 @@ class my_navsat_transform:
     def set_origin(self, origin):
         self.origin = origin
 
+
     def transform_to_odometry(self, navsatfix_msg):
-        y_coordinate = utils.get_distance_between_longlat(
-            self.origin,
-            utils.longlat_position(
-                self.origin.longitude, # x
-                navsatfix_msg.latitude # y
-            )
-        )
-        if navsatfix_msg.latitude < self.origin.latitude:
-            y_coordinate *= -1
-        x_coordinate = utils.get_distance_between_longlat(
-            self.origin,
-            utils.longlat_position(
-                navsatfix_msg.longitude, # x
-                self.origin.latitude # y
-            )
-        )
-        if navsatfix_msg.longitude < self.origin.longitude:
-            x_coordinate *= -1
-        # add gaussian noise with s.d. 1m
-        noise = numpy.random.normal(loc=0, scale=0.2, size=2)
-        # x_coordinate += noise[0]
-        # y_coordinate += noise[1]
+        position = utils.longlat_position(navsatfix_msg.longitude, navsatfix_msg.latitude)
+        xy_position = self.longlat_to_xy(position)
+        # add gaussian noise
+        x_coordinate = xy_position.x
+        y_coordinate = xy_position.y
+        noise = numpy.random.normal(loc=0, scale=0.1, size=2)
+        x_coordinate += noise[0]
+        y_coordinate += noise[1]
         self.seq += 1
         odom_msg = Odometry()
         odom_msg.header.seq = self.seq
@@ -58,8 +45,29 @@ class my_navsat_transform:
         odom_msg.twist.twist.angular.y = 0
         odom_msg.twist.twist.angular.z = 0
         cov = [0 for i in range(36)]
-        cov[0] = 0.2**2#(navsatfix_msg.position_covariance[0] * 100000)**2
-        cov[7] = 0.2**2#(navsatfix_msg.position_covariance[4] * 100000)**2
+        cov[0] = 0.1**2#(navsatfix_msg.position_covariance[0] * 100000)**2
+        cov[7] = 0.1**2#(navsatfix_msg.position_covariance[4] * 100000)**2
         cov[14] = navsatfix_msg.position_covariance[8]
         odom_msg.pose.covariance = cov
         return odom_msg
+
+    def longlat_to_xy(self, longlat_pos):
+        y_coordinate = utils.get_distance_between_longlat(
+            self.origin,
+            utils.longlat_position(
+                self.origin.longitude, # x
+                longlat_pos.latitude # y
+            )
+        )
+        if longlat_pos.latitude < self.origin.latitude:
+            y_coordinate *= -1
+        x_coordinate = utils.get_distance_between_longlat(
+            self.origin,
+            utils.longlat_position(
+                longlat_pos.longitude, # x
+                self.origin.latitude # y
+            )
+        )
+        if longlat_pos.longitude < self.origin.longitude:
+            x_coordinate *= -1
+        return utils.xy_position(x_coordinate, y_coordinate)
