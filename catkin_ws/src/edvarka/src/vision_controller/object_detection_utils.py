5707e2804e6c3fdfa00f23864f75ffb4eebc59a0
tsioftas <panaevri@gmail.com>
2021-03-13 18:13:34 +0000

    - Fixed thresholding for object detection

--- catkin_ws/src/edvarka/src/vision_controller/object_detection_utils.py
+++ catkin_ws/src/edvarka/src/vision_controller/object_detection_utils.py
@@ -4,7 +4,7 @@ import numpy as np
 # high parameters
 IMG_SIZE = (256,256)
 X_CENTER = int(256//2)
-OBJECT_THRESH = int((256*256) * 0.1)
+OBJECT_THRESH = int((256*256) * 0.05)
 
 ################################################################################################
 # object segmentation functions
@@ -19,23 +19,22 @@ def convertUnit8(img):
 
 # method to sbtract base image from object image
 def image_subtraction(img1,img2):
-    #convert images to uint8 as descibed in the paper
-    #img1 = img1[:180][:]
-    #img2 = img2[:][:280]
-    #print(img1.shape)
-    #print(img2.shape)
-    img3 = convertUnit8(img1)
-    img4 = convertUnit8(img2)
-    return  cv2.subtract(img3,img4)
+    sub =  cv2.subtract(img1,img2)
+    sub -= np.min(sub)
+    # cv2.imshow("sub", sub)
+    # cv2.waitKey(1)
+    return sub
 
 # method to segment image using threshold
 def binarization(img):
-    #convert to grayscale
-    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-    # Otsu's thresholding after Gaussian filtering
-    blur = cv2.GaussianBlur(gray,(5,5),0)
-    ret,threshold = cv2.threshold(blur,0,255,cv2.THRESH_BINARY+cv2.THRESH_OTSU)
-    return threshold
+    t = 30
+    red = img[:,:,2] < t
+    green = img[:,:,1] < t
+    blue = img[:,:,0] < t
+    mask = red & green & blue
+    binary = np.zeros((256, 256))
+    binary[~mask] = 255
+    return binary
 
 # method to normarize the image (RGB normalizations)
 def normalize(img):
@@ -44,19 +43,18 @@ def normalize(img):
 
 # method to get segmented image from base and object images
 def segment_object(base_img,obj_img):
-    base_img = normalize(cv2.resize(base_img,IMG_SIZE))
-    obj_img = normalize(cv2.resize(obj_img,IMG_SIZE))
     seg_img = binarization(image_subtraction(obj_img,base_img))
     return seg_img
 
 # filter the segmented image
 def get_main_object(seg_img):
-    imge = cv2.erode(seg_img,np.ones((8,8),np.uint8))
+    imge = cv2.erode(seg_img,np.ones((1,1),np.uint8))
     return imge
 
 # decide object exist or not from segmented image
 def object_exist(seg_img):
     pixels = seg_img.reshape(-1)
+    print(pixels)
     white_pixels_num = len([p for p in pixels if p == 255])
     return True if white_pixels_num > OBJECT_THRESH else False
 
